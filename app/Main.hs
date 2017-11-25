b52e5626b2741777ceae4e74f80212e7950d90a8
masatoko <masatokodo@gmail.com>
2017-11-26 08:18:58 +0900

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -21,13 +21,13 @@ import           Kurokos                (Joystick, KurokosT, Metapad, Render,
                                          Scene (..), SceneState (..), Update,
                                          addAction, newPad, runKurokos,
                                          runScene, withKurokos)
-import qualified Kurokos                as P
+import qualified Kurokos                as K
 
 data Title = Title
 
 data Game = Game
-  { gSprite    :: P.Sprite
-  , gImgSprite :: P.Sprite
+  { gSprite    :: K.Sprite
+  , gImgSprite :: K.Sprite
   , gDeg       :: !Double
   , gCount     :: !Int
   , gActions   :: [Action]
@@ -36,14 +36,14 @@ data Game = Game
 allocGame :: ResourceT (KurokosT IO) Game
 allocGame = do
   liftIO . putStrLn $ "allocGame"
-  env <- lift P.getEnv
-  (_, font) <- allocate (P.loadFont fontPath 50)
-                        P.freeFont
-  (_, img) <- allocate (liftIO $ P.runKurokosEnvT env $ P.loadSprite "_data/img.png" (pure 48))
-                       (\a -> P.freeSprite a >> liftIO (putStrLn "free img.png"))
-  (_, char) <- allocate (liftIO $ P.runKurokosEnvT env $ P.newSprite font (V4 255 255 255 255) "@")
-                        (\a -> P.freeSprite a >> liftIO (putStrLn "free font sprite"))
-  -- lift $ P.withRenderer $ \r -> doSomething
+  env <- lift K.getEnv
+  (_, font) <- allocate (K.loadFont fontPath 50)
+                        K.freeFont
+  (_, img) <- allocate (liftIO $ K.runKurokosEnvT env $ K.loadSprite "_data/img.png" (pure 48))
+                       (\a -> K.freeSprite a >> liftIO (putStrLn "free img.png"))
+  (_, char) <- allocate (liftIO $ K.runKurokosEnvT env $ K.newSprite font (V4 255 255 255 255) "@")
+                        (\a -> K.freeSprite a >> liftIO (putStrLn "free font sprite"))
+  -- lift $ K.withRenderer $ \r -> doSomething
   return $ Game char img 0 0 []
   where
     fontPath = "_data/system.ttf"
@@ -53,24 +53,24 @@ main = do
   as <- getArgs
   let opt = (`elem` as)
       conf = mkConf (opt "button") (opt "axis") (opt "hat")
-      -- conf' = conf {P.confFont = Left fontBytes}
-      conf' = conf {P.confFont = Right "_data/system.ttf"}
+      -- conf' = conf {K.confFont = Left fontBytes}
+      conf' = conf {K.confFont = Right "_data/system.ttf"}
   withKurokos conf' $ \kuro -> do
-    mjs <- P.newJoystickAt 0
+    mjs <- K.newJoystickAt 0
     let gamepad = mkGamepad mjs
     _ <- runKurokos kuro $
       runScene $ titleScene mjs gamepad
-    maybe (return ()) P.freeJoystick mjs
+    maybe (return ()) K.freeJoystick mjs
     return ()
   where
     mkConf pBtn pAxis pHat =
-      P.defaultConfig
-        { P.confWinSize = V2 640 480
-        , P.confWinTitle = "protpnic-app"
-        -- , P.confWindowMode = SDL.Fullscreen
-        , P.confWindowMode = SDL.Windowed
-        , P.confDebugPrintSystem = True
-        , P.confDebugJoystick = P.DebugJoystick pBtn pAxis pHat
+      K.defaultConfig
+        { K.confWinSize = V2 640 480
+        , K.confWinTitle = "protpnic-app"
+        -- , K.confWindowMode = SDL.Fullscreen
+        , K.confWindowMode = SDL.Windowed
+        , K.confDebugPrintSystem = True
+        , K.confDebugJoystick = K.DebugJoystick pBtn pAxis pHat
         }
 
     -- monitor mjs =
@@ -79,7 +79,7 @@ main = do
     --     Just js -> forever $ do
     --       clearScreen
     --       SDL.pumpEvents
-    --       P.monitorJoystick js
+    --       K.monitorJoystick js
     --       threadDelay 100000
 
 data Action
@@ -99,33 +99,33 @@ data Action
 mkGamepad :: Maybe Joystick -> Metapad Action
 mkGamepad mjs = flip execState newPad $ do
   -- Keyboard
-  modify . addAction $ P.released SDL.ScancodeF Go
-  modify . addAction $ P.pressed SDL.ScancodeReturn Enter
-  modify . addAction $ P.pressed SDL.ScancodeEscape Exit
+  modify . addAction $ K.released SDL.ScancodeF Go
+  modify . addAction $ K.pressed SDL.ScancodeReturn Enter
+  modify . addAction $ K.pressed SDL.ScancodeEscape Exit
   -- Joystick
   case mjs of
     Just js -> do
       -- Buttons
-      modify . addAction $ P.joyPressed js 4 Enter
-      mapM_ (modify . addAction . uncurry (P.joyPressed js))
+      modify . addAction $ K.joyPressed js 4 Enter
+      mapM_ (modify . addAction . uncurry (K.joyPressed js))
         [ (10, Go), (11, Go), (12, Go), (13, Go) ]
       -- Axes
-      modify . addAction $ P.joyAxis2 js 0 1 AxisLeft
+      modify . addAction $ K.joyAxis2 js 0 1 AxisLeft
       -- Hat
-      modify . addAction $ P.joyHat P.HDUp P.Pressed PUp
-      modify . addAction $ P.joyHat P.HDUp P.Released RUp
-      modify . addAction $ P.joyHat P.HDUp P.Holded HUp
-      modify . addAction $ P.joyHat P.HDDown P.Pressed PDown
+      modify . addAction $ K.joyHat K.HDUp K.Pressed PUp
+      modify . addAction $ K.joyHat K.HDUp K.Released RUp
+      modify . addAction $ K.joyHat K.HDUp K.Holded HUp
+      modify . addAction $ K.joyHat K.HDDown K.Pressed PDown
     Nothing -> return ()
   -- Mouse
-  modify . addAction $ P.mouseButtonAct P.ButtonLeft P.Pressed Go
-  modify . addAction $ P.mousePosAct MousePos
-  modify . addAction $ P.mouseMotionAct MouseMotion
-  modify . addAction $ P.mouseWheelAct MouseWheel
+  modify . addAction $ K.mouseButtonAct K.ButtonLeft K.Pressed Go
+  modify . addAction $ K.mousePosAct MousePos
+  modify . addAction $ K.mouseMotionAct MouseMotion
+  modify . addAction $ K.mouseWheelAct MouseWheel
   -- Touch
-  modify . addAction $ P.touchMotionAct TouchMotion
+  modify . addAction $ K.touchMotionAct TouchMotion
 
-titleScene :: Maybe P.Joystick -> Metapad Action -> Scene Title IO Action
+titleScene :: Maybe K.Joystick -> Metapad Action -> Scene Title IO Action
 titleScene mjs pad =
   Scene pad update render transit (return Title) -- (\_ -> return ())
   where
@@ -134,23 +134,23 @@ titleScene mjs pad =
 
     render :: Render Title IO
     render _ _ = do
-      P.printTest (P (V2 10 100)) (V4 0 255 255 255) "Enter - start"
-      P.printTest (P (V2 10 120)) (V4 0 255 255 255) "Escape - exit"
-      P.printTest (P (V2 10 160)) (V4 0 255 255 255) "日本語テスト"
+      K.printTest (P (V2 10 100)) (V4 0 255 255 255) "Enter - start"
+      K.printTest (P (V2 10 120)) (V4 0 255 255 255) "Escape - exit"
+      K.printTest (P (V2 10 160)) (V4 0 255 255 255) "日本語テスト"
 
     transit _ as _
-      | Enter `elem` as = P.next $ mainScene mjs pad
-      | Exit  `elem` as = P.end
-      | otherwise       = P.continue
+      | Enter `elem` as = K.next $ mainScene mjs pad
+      | Exit  `elem` as = K.end
+      | otherwise       = K.continue
 
-mainScene :: Maybe P.Joystick -> Metapad Action -> Scene Game IO Action
+mainScene :: Maybe K.Joystick -> Metapad Action -> Scene Game IO Action
 mainScene mjs pad = Scene pad update render transit allocGame
   where
     update :: Update Game IO Action
     update stt as g0 = do
-      -- when (frameCount stt `mod` 60 == 0) $ P.averageTime >>= liftIO . print
+      -- when (frameCount stt `mod` 60 == 0) $ K.averageTime >>= liftIO . print
       let alpha = fromIntegral $ frameCount stt
-      P.setAlphaMod (gImgSprite g0) alpha
+      K.setAlphaMod (gImgSprite g0) alpha
       execStateT go g0
       where
         go :: StateT Game (KurokosT IO) ()
@@ -165,17 +165,17 @@ mainScene mjs pad = Scene pad update render transit allocGame
           c <- gets gCount
           let strength = fromIntegral c * 0.2
               len = fromIntegral c * 100
-          mapM_ (\joy -> P.rumble joy strength len) mjs
+          mapM_ (\joy -> K.rumble joy strength len) mjs
         count _  = return ()
 
         setDeg = modify (\g -> g {gDeg = fromIntegral (frameCount stt `mod` 360)})
 
     render :: Render Game IO
     render sst (Game spr img d i as) = do
-      P.clearBy $ V4 0 0 0 255
-      -- P.renderS spr (P (V2 150 200)) Nothing (Just d)
-      -- P.renderS img (P (V2 10 200)) Nothing Nothing
-      P.withRenderer $ \r -> do
+      K.clearBy $ V4 0 0 0 255
+      -- K.renderS spr (P (V2 150 200)) Nothing (Just d)
+      -- K.renderS img (P (V2 10 200)) Nothing Nothing
+      K.withRenderer $ \r -> do
         let p0 = V2 200 250
             p1 = p0 + (round <$> (V2 dx dy ^* 30))
               where
@@ -183,24 +183,24 @@ mainScene mjs pad = Scene pad update render transit allocGame
                 dy = sin $ fromIntegral t / 5
         Prim.thickLine r p0 p1 4 (V4 0 255 0 255)
       --
-      P.printTest (P (V2 10 100)) color "Press Enter key to pause"
-      P.printTest (P (V2 10 120)) color "Press F key!"
+      K.printTest (P (V2 10 100)) color "Press Enter key to pause"
+      K.printTest (P (V2 10 120)) color "Press F key!"
       let progress = replicate i '>' ++ replicate (targetCount - i) '-'
-      P.printTest (P (V2 10 140)) color $ T.pack progress
-      P.printTest (P (V2 10 160)) color $ T.pack $ show as
+      K.printTest (P (V2 10 140)) color $ T.pack progress
+      K.printTest (P (V2 10 160)) color $ T.pack $ show as
       where
         color = V4 255 255 255 255
         t = frameCount sst
 
     transit _ as g
-      | cnt > targetCount = P.next $ clearScene mjs cnt pad
-      | Enter `elem` as   = P.push $ pauseScene pad
+      | cnt > targetCount = K.next $ clearScene mjs cnt pad
+      | Enter `elem` as   = K.push $ pauseScene pad
       --
-      | PUp   `elem` as   = P.next $ mainScene mjs pad
-      | PDown `elem` as   = P.push $ mainScene mjs pad
-      | Exit  `elem` as   = P.end
+      | PUp   `elem` as   = K.next $ mainScene mjs pad
+      | PDown `elem` as   = K.push $ mainScene mjs pad
+      | Exit  `elem` as   = K.end
       --
-      | otherwise         = P.continue
+      | otherwise         = K.continue
       where
         cnt = gCount g
 
@@ -212,24 +212,24 @@ pauseScene pad = Scene pad update render transit allocGame
     update _ _ = return
 
     render _ _ = do
-      P.clearBy $ V4 50 50 0 255
-      P.printTest (P (V2 10 100)) (V4 255 255 255 255) "PAUSE"
+      K.clearBy $ V4 50 50 0 255
+      K.printTest (P (V2 10 100)) (V4 255 255 255 255) "PAUSE"
 
     transit _ as _
-      | Enter `elem` as = P.end
-      | otherwise       = P.continue
+      | Enter `elem` as = K.end
+      | otherwise       = K.continue
 
-clearScene :: Maybe P.Joystick -> Int -> Metapad Action -> Scene Game IO Action
+clearScene :: Maybe K.Joystick -> Int -> Metapad Action -> Scene Game IO Action
 clearScene mjs score pad = Scene pad update render transit allocGame
   where
     update _ _ = return
 
     render _ _ = do
-      P.clearBy $ V4 0 0 255 255
-      P.printTest (P (V2 10 100)) (V4 255 255 255 255) "CLEAR!"
-      P.printTest (P (V2 10 120)) (V4 255 255 255 255) $ T.pack ("Score: " ++ show score)
-      P.printTest (P (V2 10 140)) (V4 255 255 255 255) "Enter - title"
+      K.clearBy $ V4 0 0 255 255
+      K.printTest (P (V2 10 100)) (V4 255 255 255 255) "CLEAR!"
+      K.printTest (P (V2 10 120)) (V4 255 255 255 255) $ T.pack ("Score: " ++ show score)
+      K.printTest (P (V2 10 140)) (V4 255 255 255 255) "Enter - title"
 
     transit _ as _g
-      | Enter `elem` as = P.next $ titleScene mjs pad
-      | otherwise       = P.continue
+      | Enter `elem` as = K.next $ titleScene mjs pad
+      | otherwise       = K.continue
