d5b313978135624f1b4a76c6e51d493e1703584f
masatoko <masatokodo@gmail.com>
2017-11-29 09:49:47 +0900

    Refactor

--- src/Kurokos/GUI/Update.hs
+++ src/Kurokos/GUI/Update.hs
@@ -1,18 +1,24 @@
 module Kurokos.GUI.Update where
 
+import Control.Monad (foldM)
+
 import           Kurokos.GUI.Import
 import           Kurokos.GUI.Core
 
 import qualified SDL
 
-update :: (RenderEnv m, HasEvent m, MonadIO m, MonadMask m) => GUI -> m ()
-update gui = do
-  es <- getEvents
-  win <- getWindow
-  let pWinResized = any (isWinResized win) es
-  when pWinResized $ do
-    resetTexture gui
-    updateTexture gui
+update :: (RenderEnv m, HasEvent m, MonadIO m, MonadMask m) => GUI -> m GUI
+update gui = foldM workEvent gui =<< getEvents
+
+workEvent :: (RenderEnv m, HasEvent m, MonadIO m, MonadMask m) => GUI -> SDL.EventPayload -> m GUI
+workEvent gui e =
+  case e of
+   (SDL.WindowResizedEvent dt) -> doWhenResized dt
+   _ -> return gui
   where
-    isWinResized curWin (SDL.WindowResizedEvent dt) = SDL.windowResizedEventWindow dt == curWin
-    isWinResized _ _ = False
+    doWhenResized dt = do
+      win <- getWindow
+      when (SDL.windowResizedEventWindow dt == win) $ do
+        resetTexture gui
+        updateTexture gui
+      return gui
