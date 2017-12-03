6fba66847f68e71605f9411571124604e07ab379
masatoko <masatokodo@gmail.com>
2017-12-03 10:10:11 +0900

    Refactor

--- kurokos-archive/src-demo/Main.hs
+++ kurokos-archive/src-demo/Main.hs
@@ -2,46 +2,32 @@
 
 module Main where
 
-import System.FilePath.Posix
-import qualified Data.Map.Strict as M
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Map.Strict       as M
+import qualified Data.Text             as T
+import qualified Data.Text.IO          as T
+import           System.FilePath.Posix
 
-import Kurokos.Archive
+import qualified Kurokos.Archive       as ARC
+
+arcPath :: FilePath
+arcPath = "_archived/result.arc"
+
+key :: B.ByteString
+key = "secret"
 
 main :: IO ()
 main = do
-  B.putStrLn . decode key . encode key $ "KONNICHIWA!"
-  -- Archive files
-  archiveF key arcPath "_target" isValidPath
-  -- Extract files
-  extractFiles key arcPath "_extracted"
-  putStrLn "\n=== showFiles ==="
-  showFiles key arcPath >>= putStrLn
-  putStrLn "\n=== text1.txt ==="
-  readArchiveText key arcPath "text1.txt" >>= T.putStrLn
-  putStrLn "\n=== child/text2.txt ==="
-  readArchiveStr key arcPath "child/text2.txt" >>= putStrLn
-  readArchiveStr key arcPath "child/child1/../text2.txt" >>= putStrLn
-  putStrLn "\n=== directoryFiles ==="
-  directoryFiles key arcPath "child" >>= print
-  putStrLn "\n=== directoryDirs ==="
-  directoryDirs key arcPath "child" >>= print
-  directoryDirs key arcPath "" >>= print
-  putStrLn "\n=== directoryFiles ==="
-  directoryFiles key arcPath "child" >>= print
-  directoryFiles key arcPath "" >>= print
+  B.putStrLn . ARC.decode key . ARC.encode key $ "KONNICHIWA!" -- Test decode and encode
+  archiveFiles
+  extract
+  readFromArc
 
-  -- Read data from the archive file
-  putStrLn "\n=== Read from archive ==="
-  arc <- readArchive key arcPath
-  bs <- getFileBS key "child/child1/txt_child1.txt" arc
-  print bs
+-- Archive files
+archiveFiles :: IO ()
+archiveFiles =
+  ARC.archiveF key arcPath "_target" isValidPath
   where
-    arcPath = "_archived/result.arc"
-    key = "secret"
-
     isValidPath path =
       isHeadValid name && all isValidDir dirs
       where
@@ -53,3 +39,31 @@ main = do
         dirs = splitDirectories . dropFileName $ path
         isValidDir ('_':_) = False
         isValidDir _       = True
+
+-- Extract archive
+extract :: IO ()
+extract = do
+  ARC.extractFiles key arcPath "_extracted"
+  putStrLn "\n=== showFiles ==="
+  ARC.showFiles key arcPath >>= putStrLn
+  putStrLn "\n=== text1.txt ==="
+  ARC.readArchiveText key arcPath "text1.txt" >>= T.putStrLn
+  putStrLn "\n=== child/text2.txt ==="
+  ARC.readArchiveStr key arcPath "child/text2.txt" >>= putStrLn
+  ARC.readArchiveStr key arcPath "child/child1/../text2.txt" >>= putStrLn
+  putStrLn "\n=== directoryFiles ==="
+  ARC.directoryFiles key arcPath "child" >>= print
+  putStrLn "\n=== directoryDirs ==="
+  ARC.directoryDirs key arcPath "child" >>= print
+  ARC.directoryDirs key arcPath "" >>= print
+  putStrLn "\n=== directoryFiles ==="
+  ARC.directoryFiles key arcPath "child" >>= print
+  ARC.directoryFiles key arcPath "" >>= print
+
+-- Read an archive file as 'Archive' data
+readFromArc :: IO ()
+readFromArc = do
+  putStrLn "\n=== Read archive ==="
+  arc <- ARC.readArchive key arcPath -- read an archive file and make 'Archive' data
+  bs <- ARC.getFileBS key "child/child1/txt_child1.txt" arc -- read data from the Archive data
+  print bs
