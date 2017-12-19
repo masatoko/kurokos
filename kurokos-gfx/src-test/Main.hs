d215a925b3d49949b98fc3c81a6627d6a6e237e6
masatoko <masatokodo@gmail.com>
2017-12-19 18:13:22 +0900

    Use opengl

--- kurokos-gfx/src-test/Main.hs
+++ kurokos-gfx/src-test/Main.hs
@@ -2,15 +2,24 @@
 {-# LANGUAGE RecordWildCards   #-}
 module Main where
 
-import           Control.Monad (unless)
+import           Control.Monad             (unless)
 import qualified SDL
 import           SDL.Event
+
+import qualified Graphics.GL               as GLRaw
+import qualified Graphics.GLUtil           as GLU
+import           Graphics.Rendering.OpenGL (get, ($=))
+import qualified Graphics.Rendering.OpenGL as GL
 
 main :: IO ()
 main = do
   SDL.initializeAll
   window <- SDL.createWindow "Test kurokos-gfx" winConf
+  glContext <- SDL.glCreateContext window
+  SDL.swapInterval $= SDL.SynchronizedUpdates
+  GL.clearColor $= GL.Color4 0 1 0 1
   loop window
+  SDL.glDeleteContext glContext
   where
     winConf = SDL.defaultWindow {SDL.windowOpenGL = Just glConf}
 
@@ -24,6 +33,8 @@ main = do
         go = do
           events <- SDL.pollEvent
           --
+          GL.clear [GL.ColorBuffer]
+          SDL.glSwapWindow w
           --
           unless (any shouldExit events) go
 
@@ -32,4 +43,5 @@ main = do
         KeyboardEvent KeyboardEventData{..} ->
           keyboardEventKeyMotion == Pressed &&
             SDL.keysymKeycode keyboardEventKeysym == SDL.KeycodeQ
+        WindowClosedEvent WindowClosedEventData{} -> True
         _ -> False
