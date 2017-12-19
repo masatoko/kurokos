641ff67f403869d807ce0b9d013547bbbfab7429
masatoko <masatokodo@gmail.com>
2017-12-19 17:54:25 +0900

    SDL loop

--- kurokos-gfx/src-test/Main.hs
+++ kurokos-gfx/src-test/Main.hs
@@ -1,4 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Main where
 
+import           Control.Monad (unless)
+import qualified SDL
+import           SDL.Event
+
 main :: IO ()
-main = undefined
+main = do
+  SDL.initializeAll
+  window <- SDL.createWindow "Test kurokos-gfx" winConf
+  loop window
+  where
+    winConf = SDL.defaultWindow {SDL.windowOpenGL = Just glConf}
+
+    glConf =
+      SDL.defaultOpenGL
+        { SDL.glProfile = SDL.Core SDL.Debug 4 0
+        }
+
+    loop w = go
+      where
+        go = do
+          events <- SDL.pollEvent
+          --
+          --
+          unless (any shouldExit events) go
+
+    shouldExit e =
+      case SDL.eventPayload e of
+        KeyboardEvent KeyboardEventData{..} ->
+          keyboardEventKeyMotion == Pressed &&
+            SDL.keysymKeycode keyboardEventKeysym == SDL.KeycodeQ
+        _ -> False
