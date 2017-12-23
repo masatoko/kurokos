50ddfe071e4c061a36e41397871835ad48083378
masatoko <masatokodo@gmail.com>
2017-12-23 10:58:07 +0900

    Comment out

--- src/Kurokos/Texture.hs
+++ src/Kurokos/Texture.hs
@@ -1,30 +1,28 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-module Kurokos.Texture
-  (
-  -- ** Texture State
-    setBlendMode
-  , setAlphaMod
-  , setColorMod
-  ) where
-
-import           Control.Monad.Reader
-import           Data.Word                    (Word8)
-import           Linear.V3
-
-import           SDL                          (($=))
-import qualified SDL
-
--- State
-
-setBlendMode :: MonadIO m => SDL.Texture -> SDL.BlendMode -> m ()
-setBlendMode t mode =
-  SDL.textureBlendMode t $= mode
-
-setAlphaMod :: MonadIO m => SDL.Texture -> Word8 -> m ()
-setAlphaMod t alpha =
-  SDL.textureAlphaMod t $= alpha
-
-setColorMod :: MonadIO m => SDL.Texture -> V3 Word8 -> m ()
-setColorMod t rgb =
-  SDL.textureColorMod t $= rgb
+-- module Kurokos.Texture where
+--   (
+--   -- ** Texture State
+--     setBlendMode
+--   , setAlphaMod
+--   , setColorMod
+--   ) where
+--
+-- import           Control.Monad.Reader
+-- import           Data.Word                    (Word8)
+-- import           Linear.V3
+--
+-- import           SDL                          (($=))
+-- import qualified SDL
+--
+-- setBlendMode :: MonadIO m => SDL.Texture -> SDL.BlendMode -> m ()
+-- setBlendMode t mode =
+--   SDL.textureBlendMode t $= mode
+--
+-- setAlphaMod :: MonadIO m => SDL.Texture -> Word8 -> m ()
+-- setAlphaMod t alpha =
+--   SDL.textureAlphaMod t $= alpha
+--
+-- setColorMod :: MonadIO m => SDL.Texture -> V3 Word8 -> m ()
+-- setColorMod t rgb =
+--   SDL.textureColorMod t $= rgb
