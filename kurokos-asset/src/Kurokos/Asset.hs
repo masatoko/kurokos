3ee2e3306d61db0b004008287d8e1c04052f4efd
masatoko <masatokodo@gmail.com>
2018-01-31 15:43:30 +0900

    AssetManager allocAssetManager

--- kurokos-asset/src/Kurokos/Asset.hs
+++ kurokos-asset/src/Kurokos/Asset.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Kurokos.Asset
   (
   -- ** Types
     AssetManager
   -- ** Generate
+  , allocAssetManager
   , newAssetManager
   , freeAssetManager
   -- ** Get
@@ -16,6 +18,7 @@ import           Control.Concurrent.MVar
 import qualified Control.Exception            as E
 import           Control.Monad                (foldM)
 import           Control.Monad.IO.Class       (MonadIO, liftIO)
+import           Control.Monad.Trans.Resource
 import qualified Data.ByteString              as BS
 import           Data.Char                    (toLower)
 import qualified Data.Map                     as M
@@ -35,6 +38,30 @@ data AssetManager = AssetManager
   , amTexMap  :: M.Map Ident G.Texture
   }
 
+allocAssetManager :: (MonadIO m, MonadThrow m, MonadBaseControl IO m) => G.Renderer -> RawAssetManager -> ResourceT m AssetManager
+allocAssetManager rndr (RawAssetManager bmap) =
+  foldM work empty $ M.toList bmap
+  where
+    ft = G.getFreeType rndr
+    empty = AssetManager M.empty M.empty M.empty
+
+    work am@AssetManager{..} (ident, (path, bytes)) = update
+      where
+        ext = filter (/= '.') . map toLower . takeExtension $ path
+        update
+          | ext == "ttf" = do -- TODO: Add extensions
+              font <- snd <$> allocate (Font.newFaceBS ft bytes) Font.doneFace
+              return $ am {amFontMap = M.insert ident font amFontMap}
+          | S.member ext imageExts = do
+              tex <- snd <$> allocate (G.decodeTexture bytes) G.deleteTexture
+              return $ am {amTexMap = M.insert ident tex amTexMap}
+          | otherwise =
+              return $ am {amByteMap = M.insert ident bytes amByteMap}
+
+    imageExts :: S.Set String
+    imageExts = S.fromList ["png", "jpg", "jpeg", "gif", "pic", "hdr", "tga", "tiff"]
+
+-- TODO: Remove this. Font is released unexpectedly.
 newAssetManager :: MonadIO m => G.Renderer -> RawAssetManager -> m AssetManager
 newAssetManager rndr (RawAssetManager bmap) = liftIO $
   foldM work empty $ M.toList bmap
@@ -58,6 +85,7 @@ newAssetManager rndr (RawAssetManager bmap) = liftIO $
     imageExts :: S.Set String
     imageExts = S.fromList ["png", "jpg", "jpeg", "gif", "pic", "hdr", "tga", "tiff"]
 
+-- TODO: Remove this.
 freeAssetManager :: MonadIO m => AssetManager -> m ()
 freeAssetManager AssetManager{..} = liftIO $ do
   mapM_ G.deleteTexture $ M.elems amTexMap
